678d85e8823f690943e6875ee12d6903673dcdfe
mbd-s <michaelbsilverberg@gmail.com>
2016-04-15 15:04:59 -0700

    index and indiv book pages up

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,14 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+  var endpoint = 'https://super-crud.herokuapp.com/books';
+
+  $http({
+    method: 'GET',
+    url: endpoint
+  }).then(function successCallback(response){
+    vm.books = response.data.books;
+  }, function errorCallback(response){
+    console.log('Error getting the books', response);
+  });
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,17 @@
-angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+angular
+  .module('libraryApp')
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  vm.newBook = {};
+  var endpoint = 'https://super-crud.herokuapp.com/books';
+
+  $http({
+    method: 'GET',
+    url: endpoint + '/' + $routeParams.id
+  }).then(function successCallback(json){
+    vm.book = json.data;
+  });
+}
