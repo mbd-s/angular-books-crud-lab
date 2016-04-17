820c3a41e63d4a819b6fbf94c6c5c16065fc0ed5
mbd-s <michaelbsilverberg@gmail.com>
2016-04-17 11:40:34 -0700

    can now create a book

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,24 +4,28 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
-  var endpoint = 'https://super-crud.herokuapp.com/books';
+  vm.newBook = {};
   $http({
     method: 'GET',
-    url: endpoint
+    url: 'https://super-crud.herokuapp.com/books'
   }).then(function successCallback(response){
     vm.books = response.data.books;
   }, function errorCallback(response){
     console.log('Error getting the books', response);
   });
 
-//TODO write addBook() function
-  // $http({
-  //   method: 'POST',
-  //   url: endpoint
-  // }).then(function successCallback(response){
-  //   vm.books = response.data.books;
-  // }, function errorCallback(response){
-  //   console.log('Error saving the book', response);
-  // });
+  vm.createBook = function(){
+      $http({
+        method: 'POST',
+        url: 'https://super-crud.herokuapp.com/books',
+        data: vm.newBook
+      }).then(function onBookCreateSuccess(response){
+        vm.books.push(response.data);
+        console.log('New book successfully created');
+      }, function errorCallback(response){
+        console.log('Error saving the book', response);
+      });
+    }
+
 
 }
